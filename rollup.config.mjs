3ef35a40f0a5838da702dd68e4989f1a1eb15826
sam585456525 <sam585456525@gmail.com>
2023-09-24 00:28:52 +0800

    add: build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,16 +1,18 @@
 import { defineConfig } from "rollup";
 import typescript from "@rollup/plugin-typescript";
 import del from 'rollup-plugin-delete'
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default defineConfig({
   plugins: [
+    nodeResolve(),
     del({ targets: 'dist/*' }),
     typescript({ tsconfig: "./tsconfig.build.json" })
   ],
-  external: ['googleapis'],
+  external: ['formik'],
   input: "src/index.ts",
   output: {
     file: "dist/index.js",
-    format: "cjs",
+    format: "esm"
   },
 });
