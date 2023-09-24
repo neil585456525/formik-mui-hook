b69d85adb9fcdb3708781dfa9bae066bdba60106
sam585456525 <sam585456525@gmail.com>
2023-09-24 10:43:33 +0800

    edit: build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,14 +2,16 @@ import { defineConfig } from "rollup";
 import typescript from "@rollup/plugin-typescript";
 import del from 'rollup-plugin-delete'
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   plugins: [
+    visualizer(),
     nodeResolve(),
     del({ targets: 'dist/*' }),
-    typescript({ tsconfig: "./tsconfig.build.json" })
+    typescript({ tsconfig: "./tsconfig.build.json" }),
   ],
-  external: ['formik'],
+  external: ['formik', 'yup'],
   input: "src/index.ts",
   output: {
     file: "dist/index.js",
